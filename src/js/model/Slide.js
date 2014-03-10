9f7e2b933b25bd0ebc906f5444ec3a10c06e3680
Matthias Hoelzl <tc@xantira.com>
2014-03-10 17:59:25 +0100

    Fix cloning of slides

--- src/js/model/Slide.js
+++ src/js/model/Slide.js
@@ -36,8 +36,25 @@ export class Slide extends EventEmitter {
    }
 
    duplicate () {
+      var objects = this.objects;
+      // console.log('duplicate()', this, objects, objects.length);
+
+      // Need to populate objects in this convoluted manner since #clone may be
+      // async, and the result for async objects is undefined, whereas the
+      // callback is not invoked for objects for which clone is synchronous.
+      var clonedObjects = [];
+
+      for (var i = 0, len = objects.length; i < len; i++) {
+         var newObj = objects[i].clone(c => {
+            clonedObjects.push(c);
+         });
+         if (newObj) {
+            clonedObjects.push(newObj);
+         }
+      }
+
       return new Slide(`Duplicate of ${this.title}`, this.text, this.thumbnail,
-                       _.map(this.objects, obj => obj.clone(x => x)));
+                       clonedObjects);
    }
 }
 
